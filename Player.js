9b8e5d8079761fbaafd5abf64a4af8381ed44abb
John Whiles <john.whiles@contentful.com>
2021-02-19 10:16:42 +0100

    sometimes all in

--- Player.js
+++ Player.js
@@ -3,8 +3,14 @@ class Player {
     return '0.1';
   }
 
-  static betRequest(gameState, bet) {
-    bet(0);
+  betRequest(gameState, bet) {
+    // const { players, current_buy_in }
+
+    // const callAmount = current_buy_in - players[in_action][bet]
+
+    const betOrNot = Math.floor(Math.random() * 2)
+
+    betOrNot ? bet(100000000) : bet(0)
   }
 
   static showdown(gameState) {
