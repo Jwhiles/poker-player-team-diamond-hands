0688c92cdbf7e14e62f33e7561491a8bfc08442c
John Whiles <john.whiles@contentful.com>
2021-02-19 10:34:52 +0100

    continue betting if we have already

--- Player.js
+++ Player.js
@@ -1,22 +1,47 @@
 class Player {
   static get VERSION() {
-    return '0.1';
+    return "0.1";
   }
 
   betRequest(gameState, bet) {
-    const { players, in_action, current_buy_in, minimum_raise } = gameState
+    const { players, in_action, current_buy_in, minimum_raise } = gameState;
 
-    const callAmount = current_buy_in - players[in_action]['bet']
+    const callAmount = current_buy_in - players[in_action]["bet"];
 
-    const currentBuyIn = current_buy_in - players[in_action]['bet'] + minimum_raise
+    const currentBuyIn = current_buy_in - players[in_action]["bet"] + minimum_raise;
 
-    const betOrNot = Math.floor(Math.random() * 2)
+    const us = this.getOurPlayer(gameState);
 
-    betOrNot ? bet(callAmount) : bet(0)
+    const haveWeAlreadyBet = us.bet > 0;
+
+    const betOrNot = Math.floor(Math.random() * 2);
+
+    haveWeAlreadyBet ? bet(callAmount) : betOrNot ? bet(callAmount) : bet(0);
   }
 
-  static showdown(gameState) {
+  getOurPlayer(gameState) {
+    return gameState.players.find(player => {
+      return player.name === "Team Diamond Hands";
+    });
   }
+
+  // doWeHaveAGoodHand(communityCards, privateCards) {
+  //   const allCards =  communityCards.concat(privateCards)
+
+  //   allCards.reduce((acc, card) => {
+  //     if (acc[card.rank]) {
+  //       acc[card.rank] += 1
+  //     } else {
+  //       acc[card.rank] = 1
+  //     }
+  //     return acc
+  //   }, {})
+
+  //   for (const
+
+  // }
+
+  static showdown(gameState) {}
 }
 
 module.exports = Player;
